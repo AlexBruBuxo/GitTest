2c03160ab7357a61a28fba88c6e4d82032af8a39
AlexBruBuxo <brubuxo@gmail.com>
2018-09-16 01:20:43 +0200

    initial commit

---
+++ app/src/main/java/com/alex/bru/helloworld/MainActivity.java
@@ -0,0 +1,27 @@
+package com.alex.bru.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+    }
+
+    public void clickBoto(View view){
+        Toast.makeText(this, "holi", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+}
